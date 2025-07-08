789aa8b9e020ba87fdbae7a3646325686b626455
Heeman <himanshuholmes@gmail.com>
2025-07-08 18:59:28 +0530

    add helper function to check right args

--- github-api/list-users.sh
+++ github-api/list-users.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-
+function helper {
+  expected_cmd_args=2
+  if [ $# -ne $expected_cmd_args ]; then
+	  echo "Please execute the script with required cmd args"
+	  echo "asd"
+	  exit 1
+  fi
+}
+helper "$#"
 # GitHub API URL
 API_URL="https://api.github.com"
 
@@ -42,3 +50,5 @@ function list_users_with_read_access {
 
 echo "Listing users with read access to ${REPO_OWNER}/${REPO_NAME}..."
 list_users_with_read_access
+
+
